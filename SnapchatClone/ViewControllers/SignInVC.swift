76a55178a7682385cc1d1c5faef4f0054a49f807
Nihat Can Çivi <nihatcancivi@hotmail.com>
2022-03-04 10:56:35 -0800

    Firebase auth

--- SnapchatClone/ViewControllers/SignInVC.swift
+++ SnapchatClone/ViewControllers/SignInVC.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class SignInVC: UIViewController {
 
@@ -14,13 +15,50 @@ class SignInVC: UIViewController {
     @IBOutlet weak var emailText: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
 
     @IBAction func signInClicked(_ sender: Any) {
+        if emailText.text != "" && passwordText.text != "" {
+            Auth.auth().signIn(withEmail: emailText.text!, password: passwordText.text!) { authdata, error in
+                if error != nil {
+                    self.makeAlert(titleInput: "Error", messageInput: error?.localizedDescription ?? "Error")
+                }else{
+                    self.performSegue(withIdentifier: "toFeedVC", sender: nil)
+                }
+            }
+            
+        }else{
+            self.makeAlert(titleInput: "Error", messageInput: "Username/Password/Email ? ")
+        }
     }
-    
     @IBAction func signUpClicked(_ sender: Any) {
+        if emailText.text != "" && usernameText.text != "" && passwordText.text != "" {
+            Auth.auth().createUser(withEmail: emailText.text!, password: passwordText.text!) { authdata, error in
+                if error != nil {
+                    self.makeAlert(titleInput: "Error", messageInput: error?.localizedDescription ?? "Error")
+                }else{
+                    //username i dbye kaydedicez
+                    let firestore = Firestore.firestore()
+                    
+                    let userDictionary = ["email" : self.emailText.text!, "username" : self.usernameText.text!] as [String : Any]
+                    firestore.collection("UserInfo").addDocument(data: userDictionary) { error in
+                        if error != nil {
+                            self.makeAlert(titleInput: "Error", messageInput: error?.localizedDescription ?? "Error")
+                        }
+                    }
+                    self.performSegue(withIdentifier: "toFeedVC", sender: nil)
+                }
+            }
+        }else{
+            self.makeAlert(titleInput: "Error", messageInput: "Username/Password/Email ? ")
+        }
+    }
+    func makeAlert(titleInput : String , messageInput : String) {
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
     }
 }
 
